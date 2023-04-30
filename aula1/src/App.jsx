3cab04a082cf5a5804e159c439eff3bcb50aa11a
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-04-30 19:07:37 -0300

    inicializandoReact

---
+++ aula1/src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css'
+import './caminho.css' //agora o css deve ser importado para a aplicação
+
+function App() {
+
+  return (
+    <>
+        <h1>Olá mundo!!!!</h1>
+
+      <form>
+        <input type="text" />
+        <input type="text" />
+      </form>
+
+    </>
+
+  )
+}
+
+export default App
+
+// Javasscript + XML
+// class -> className
+// for -> htmlFor
+// <img> -> <img /> fecha e abre junto da mesma 
