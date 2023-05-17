62836d579416ed4763e008f3ab9aeb48d753ae6b
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-05-17 14:51:15 -0300

    sprint2

---
+++ sprint2/aula1/aula1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
+import { RecipeteList } from './components/ReciptList'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Header />
+      <RecipeteList />
+    </>
+  )
+}
+
+export default App
---
+++ sprint2/aula1/aula1/src/components/ReciptList/index.jsx
@@ -0,0 +1,18 @@
+ export const RecipeteList = () =>{
+    return(
+
+        <>
+          <h3>Lista de receitas</h3>
+
+          <ul>
+            <li> <h4>Hamburgue</h4></li>
+            <p>hamburgue e pão</p>
+            <li> <h4>Miojo</h4></li>
+            <p>Macarrão e tempero</p>
+          </ul>
+        </>
+      
+
+        
+    )
+ }
