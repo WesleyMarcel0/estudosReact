8f984f00437dcb16c019129533f6bf8f01c0439f
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-06-06 21:52:52 -0300

    Update App.jsx

--- sprint2/aula5/projeto/src/App.jsx
+++ sprint2/aula5/projeto/src/App.jsx
@@ -1,11 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from './components/Header'
 import { CategoryList } from './components/CategoryList'
 import { NewFeed } from './components/NewFeed'
+import { api } from './Services/api';
 
 
 function App() {
  
+  const[newsList, setNewsList] = useState([]);
+  const[favoritesList, setfavoritesList] = useState([]);
+  const[categories, setCategories] = useState([]);
+
+  console.log(categories);
+
+  const loadNews = async() =>{
+    try {
+      const response = await api.get('/news');
+  
+      setNewsList(response.data);
+
+    } catch (error) {
+      console.log('erro');
+    }
+  }
+
+  const loadCategories = async() =>{
+    try {
+      const response = await api.get('/categories'); // ler a documentação da api pra usar o metodo recomendado no caso esse a documentação diz para usar get
+      setCategories(response.data);
+
+    } catch (error) {
+      console.log('erro')
+    }
+  }
+
+  useEffect(() => {
+
+    loadNews();
+    loadCategories();
+
+  },[]); // efeito de montagem
 
   return (
     <>
