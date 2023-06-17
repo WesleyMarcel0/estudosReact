530b0307fe4cd5fd7ecc2ab74bdcb9e1c6780cf1
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-06-17 14:32:25 -0300

    sprin3Inicio

---
+++ sprint3/aula1/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import { Routes , Route } from "react-router-dom";
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Teste</h1>
+
+      <Routes>
+
+        
+      </Routes>
+    </>
+  )
+}
+
+export default App
