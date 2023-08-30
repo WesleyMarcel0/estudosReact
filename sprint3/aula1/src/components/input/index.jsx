6e8b771e392fbac1768cb8f87c91035cd13b5c20
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-08-29 23:23:48 -0300

    ultima aula

--- sprint3/aula1/src/components/input/index.jsx
+++ sprint3/aula1/src/components/input/index.jsx
@@ -1,13 +1,14 @@
 import { forwardRef } from "react"
+import { Container } from "./styles";
 
  const Input = ({id,label, ... rest}, ref) => {
     //recebe a ref como segundo item sem gerar erro
     return(
-        <div>
+        <Container>
             {label ? <label htmlFor={id}>{label}</label> : null}
             <input id={id} ref={ref} {... rest} />
     
-        </div>
+        </Container>
     );
 };
 
--- sprint3/aula1/src/pages/register/index.jsx
+++ sprint3/aula1/src/pages/register/index.jsx
@@ -4,6 +4,12 @@ import { Input2 } from "../../components/input/index3Forma";
 import { useState } from "react";
 import { api } from "../../services";
 import { useNavigate } from "react-router-dom";
+import * as z from 'zod';
+
+const schema = z.object({
+    
+})
+
 export const Register = ( )=>{
 
     const {register,handleSubmit} = useForm(); //desistruturar
@@ -15,8 +21,9 @@ export const Register = ( )=>{
     const handleRegister = async (data) =>{
 
         try {
-            await api.post('/books', data)
-            navigate('/home')
+         //   await api.post('/books', data)
+         //   navigate('/home')
+            console.log(data);
 
         } catch (error) {
             console.error(error)
@@ -30,14 +37,59 @@ export const Register = ( )=>{
             <h1>registro</h1>
 
             <form onSubmit={handleSubmit(handleRegister)} >
+
+                {/*fielset aquela caixa em volta do formulario
+                   legend é uma escreta que fica no meio da linha */}
+
                 <InputRef 
-                    type='email'
-                    id='email'
-                    label='Email'
-                    placeholder='test@gmail.com'
-                    {...register('email')}
+                    type='text'
+                    id='name'
+                    label='Nome'
+                    placeholder='Nome'
+                    {...register('name')}
                 />
 
+                <InputRef 
+                    type='text'
+                    id='author'
+                    label='Autor'
+                    placeholder='Autor'
+                    {...register('author')}
+                />
+
+                <InputRef 
+                    type='text'
+                    id='cover'
+                    label='Capa'
+                    placeholder='ex: https://capa.com'
+                    {...register('cover')}
+                />
+
+                <InputRef 
+                    type='text'
+                    id='published'
+                    label='publicado em'
+                    placeholder='ex Janeiro de 2023'
+                    {...register('published')}
+                />
+                
+                <InputRef 
+                    type='number'
+                    id='numberOfPages'
+                    label='N° de Paginas'
+                    placeholder='ex: XX'
+                    {...register('numberOfPages')}
+                />
+
+                <InputRef 
+                    type='text'
+                    id='publishedCompany'
+                    label='Editora'
+                    placeholder='editora'
+                    {...register('publishedCompany')}
+                />
+
+{/*
                 <Input2
                     type={isTypePassword ? 'password' : 'text'}
                     id='password'
@@ -50,7 +102,7 @@ export const Register = ( )=>{
 
                 <input type="text" {...register('text')} />
                 
-
+*/}
                 <button type="submit">Cadastrar</button>
 
             </form>
