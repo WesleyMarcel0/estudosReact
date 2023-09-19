d1cf28e2ea6884ed136ed95b1770f1e842107e11
Wesley Marcelo <99682129+WesleyMarcel0@users.noreply.github.com>
2023-09-18 23:59:09 -0300

    aula 2 terminada

--- sprint4/aula2/src/provaiders/AutoProvider.jsx
+++ sprint4/aula2/src/provaiders/AutoProvider.jsx
@@ -1,12 +1,42 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import jwtDecode from 'jwt-decode'
 
 export const AuthContext = createContext({});
 
 export const AuthProvider = ({children}) =>{
     const [ user , setUser ] = useState(null);
+    const [loading, setloading] = useState(true);
     const navigate = useNavigate();
+
+    useEffect(()=>{
+        const loadUser = async () =>{
+            try {
+                const token = localStorage.getItem("@ts-book:token");
+
+                if (!token) {
+                  return;
+                }
+
+                const { sub } = jwtDecode(token);
+
+                const renponse = await api.get(`users/${sub}`, {// ta confirmando se o token é valido
+                  headers: {
+                    Authorization: `Bearer ${token}`,
+                  },
+                });
+
+                setUser(response.data);
+                
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setloading(false);
+            }
+        };
+
+        loadUser();
+    },[])
 
     const signIn = async (data) =>{ // sempre usar o metodo post pois ele é o mais seguro para fazer a vereficação de informaçoes sensiveis como senha e login, nunca get! ou outra forma, qualquer outra esta errada
        const response = await api.post('/login', data)
@@ -19,7 +49,7 @@ export const AuthProvider = ({children}) =>{
     }
 
     return(
-        <AuthContext.Provider value={{ signIn, user }} >
+        <AuthContext.Provider value={{ signIn, user,loading }} >
             {children}
         </AuthContext.Provider>
     )
